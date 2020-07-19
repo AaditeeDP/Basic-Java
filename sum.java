6f66a7980015248b49c8dab3285fe153c698def5
Aaditee D. Pate <62547686+AaditeeDP@users.noreply.github.com>
2020-07-19 11:39:53 +0530

    To Add 2 numbers

---
+++ sum.java
@@ -0,0 +1,12 @@
+package MyPackage;
+
+public class sum {
+    public static void main(String[] args)
+    {
+        int x = 100;
+        int y = 200;
+        System.out.println( x+y );
+        int z = x + y;
+        System.out.println( z );
+    }
+}
