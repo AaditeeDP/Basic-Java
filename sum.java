8372822cc142db8d8cb6bd33f96802c3d7f21971
Aaditee D. Pate <62547686+AaditeeDP@users.noreply.github.com>
2020-07-19 11:42:28 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -5,8 +5,8 @@ public class sum {
     {
         int x = 100;
         int y = 200;
-        System.out.println( x+y );
-        int z = x + y;
+        System.out.println( x+y ); 
+        int z = x + y; //creating a new variable and assigning the value
         System.out.println( z );
     }
 }
