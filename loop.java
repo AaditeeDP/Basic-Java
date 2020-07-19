90d721bb02569af5b9d2cf8db746c8d93c3bfa37
Aaditee D. Pate <62547686+AaditeeDP@users.noreply.github.com>
2020-07-19 11:31:47 +0530

    To understand working of for loop
    Also to observe difference between print and println

---
+++ loop.java
@@ -0,0 +1,16 @@
+package MyPackage;
+
+public class loop {
+    public static void main( String args[])
+    {
+        int x;
+        for(x=1;x<=10;x++)
+        {
+           System.out.println(x);
+        }
+        for(x=1;x<=10;x++)
+        {
+            System.out.print(x);
+        }
+    }
+}
