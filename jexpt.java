dea2da4cec4cb9efcab09c37acc1b49231998e8b
Aaditee D. Pate <62547686+AaditeeDP@users.noreply.github.com>
2020-09-02 12:12:15 +0530

    making a package

---
+++ jexpt.java
@@ -0,0 +1,10 @@
+package p;
+class expt{
+	public void ex1(){
+		System.out.println("We are in class C1 and method ex1");
+	}
+	public static void main(String args[]){
+		expt obj = new expt();
+		obj.ex1();
+	}
+}
